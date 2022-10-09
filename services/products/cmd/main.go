4b4a10f2fb34403ee110882bea09a18f0cdfad5c
meysamhadeli <meysamhadeli@gmail.com>
2022-10-10 00:54:33 +0330

    remove unnecessary code

--- services/products/cmd/main.go
+++ services/products/cmd/main.go
@@ -1,13 +1,10 @@
 package main
 
 import (
-	"flag"
-	"fmt"
 	"github.com/meysamhadeli/shop-golang-microservices/pkg/logger"
 	"github.com/meysamhadeli/shop-golang-microservices/services/products/config"
 	"github.com/meysamhadeli/shop-golang-microservices/services/products/internal/products/server"
 	"log"
-	"math/rand"
 	"os"
 )
 
@@ -15,21 +12,6 @@ const dev = "development"
 const production = "production"
 
 func main() {
-
-	a := rand.Intn(10)
-	for a < 100 {
-		if a%5 == 0 {
-			goto done
-		}
-		a = a*2 + 1
-	}
-
-	fmt.Println("do something when the loop completes normally")
-done:
-	fmt.Println("do complicated stuff no matter why we left the loop")
-	fmt.Println(a)
-
-	flag.Parse()
 
 	env := os.Getenv("APP_ENV")
 	if env == "" {
