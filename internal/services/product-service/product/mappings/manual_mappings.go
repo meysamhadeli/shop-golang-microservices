8e702dabca9de6a66061229fcebe4ea6287ac689
meysamhadeli <meysamhadeli@gmail.com>
2022-10-20 00:40:40 +0330

    retry for fix build

--- internal/services/product-service/product/mappings/manual_mappings.go
+++ internal/services/product-service/product/mappings/manual_mappings.go
@@ -1,12 +1,12 @@
 package mappings
 
 import (
-	"github.com/meysamhadeli/shop-golang-microservices/internal/services/product-service/product/dto"
+	"github.com/meysamhadeli/shop-golang-microservices/internal/services/product-service/product/dtos"
 	"github.com/meysamhadeli/shop-golang-microservices/internal/services/product-service/product/models"
 )
 
-func ProductToProductResponseDto(product *models.Product) *dto.ProductDto {
-	return &dto.ProductDto{
+func ProductToProductResponseDto(product *models.Product) *dtos.ProductDto {
+	return &dtos.ProductDto{
 		ProductId:   product.ProductId,
 		Name:        product.Name,
 		Description: product.Description,
